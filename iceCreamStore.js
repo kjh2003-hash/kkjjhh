7571079c0ce6e7bc2f18c60b8559af1f9b5ecbf4
kjh2003-hash <zxychqw12@gmail.com>
2025-09-26 15:46:14 +0900

    Update iceCreamStore.js

--- iceCreamStore.js
+++ iceCreamStore.js
@@ -1,12 +1,13 @@
-console.log("Ice Cream Store is running!");
+console.log("Ice Cream Store is running!"); 
+
 let iceCreamFlavors = [
-    { name: "Chocolate", type: "Chocolate", price: 2 },
-    { name: "Strawberry", type: "Fruit", price: 1 },
-    { name: "Vanilla", type: "Vanilla", price: 2 },
-    { name: "Pistachio", type: "Nuts", price: 1.5 },
-    { name: "Neapolitan", type: "Chocolate", price: 2},
-    { name: "Mint Chip", type: "Chocolate", price: 1.5 },
-    { name: "Raspberry", type: "Fruit", price: 1 },
+    { name: "Chocolate", type: "Chocolate", price: 2, sales: 50 },
+    { name: "Strawberry", type: "Fruit", price: 1, sales: 30 },
+    { name: "Vanilla", type: "Vanilla", price: 2, sales: 40 },
+    { name: "Pistachio", type: "Nuts", price: 1.5, sales: 20 },
+    { name: "Neapolitan", type: "Chocolate", price: 2, sales: 35 },
+    { name: "Mint Chip", type: "Chocolate", price: 1.5, sales: 25 },
+    { name: "Raspberry", type: "Fruit", price: 1, sales: 15 },
 ];
 
 // 배열 전체 출력
@@ -18,3 +19,19 @@ console.log("\nFlavor Names:");
 iceCreamFlavors.forEach(flavor => {
     console.log(flavor.name);
 });
+
+// 가장 많이 팔린 맛 찾기
+let maxSales = 0;
+let bestSelling = "";
+
+// Object.keys() 사용해서 객체의 키 탐색
+iceCreamFlavors.forEach(flavor => {
+    Object.keys(flavor).forEach(key => {
+        if (key === "sales" && flavor[key] > maxSales) {
+            maxSales = flavor[key];
+            bestSelling = flavor.name;
+        }
+    });
+});
+
+console.log(`\nBest Selling Flavor: ${bestSelling} (${maxSales} sold)`);
