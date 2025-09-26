9d755154fadcd49b00f593931a58c90299240fd7
kjh2003-hash <zxychqw12@gmail.com>
2025-09-26 15:44:01 +0900

    Update iceCreamStore.js

--- iceCreamStore.js
+++ iceCreamStore.js
@@ -1,3 +1,4 @@
+console.log("Ice Cream Store is running!");
 let iceCreamFlavors = [
     { name: "Chocolate", type: "Chocolate", price: 2 },
     { name: "Strawberry", type: "Fruit", price: 1 },
@@ -5,6 +6,15 @@ let iceCreamFlavors = [
     { name: "Pistachio", type: "Nuts", price: 1.5 },
     { name: "Neapolitan", type: "Chocolate", price: 2},
     { name: "Mint Chip", type: "Chocolate", price: 1.5 },
-    { name: "Raspberry", type: "Fruit", price: 1},
-   ];
-   
+    { name: "Raspberry", type: "Fruit", price: 1 },
+];
+
+// 배열 전체 출력
+console.log("Ice Cream Flavors:");
+console.log(iceCreamFlavors);
+
+// 이름만 따로 출력
+console.log("\nFlavor Names:");
+iceCreamFlavors.forEach(flavor => {
+    console.log(flavor.name);
+});
