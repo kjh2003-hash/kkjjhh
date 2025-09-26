4732dd530f551609c2803e8ee44d4093814d1461
kjh2003-hash <zxychqw12@gmail.com>
2025-09-26 15:58:05 +0900

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,40 +1,46 @@
+// 랜덤 카드 생성 함수 (1~11)
+function getRandomCard() {
+  return Math.floor(Math.random() * 11) + 1; // 1~11
+}
+
 // 플레이어 카드
-let cardOne = 7;
-let cardTwo = 5;
-let cardThree = 7;
+let cardOne = getRandomCard();
+let cardTwo = getRandomCard();
+let cardThree = getRandomCard();
 let playerSum = cardOne + cardTwo + cardThree;
 
 // 딜러 카드
-let cardOneBank = 7;
-let cardTwoBank = 5;
-let cardThreeBank = 6;
-let cardFourBank = 4;
-
-// 딜러 합계 계산
+let cardOneBank = getRandomCard();
+let cardTwoBank = getRandomCard();
+let cardThreeBank = getRandomCard();
+let cardFourBank = getRandomCard();
 let bankSum = cardOneBank + cardTwoBank + cardThreeBank + cardFourBank;
+
+// 결과 출력
+console.log(`Player cards: ${cardOne}, ${cardTwo}, ${cardThree}`);
+console.log(`Dealer cards: ${cardOneBank}, ${cardTwoBank}, ${cardThreeBank}, ${cardFourBank}`);
 
 // 플레이어 상태 체크
 if (playerSum > 21) {
-  console.log(`You have ${playerSum} points`);
-  console.log('Bust! You lost');
+console.log(`You have ${playerSum} points`);
+console.log('Bust! You lost');
 } else if (playerSum === 21) {
-  console.log(`You have ${playerSum} points`);
-  console.log('Blackjack! You win');
+console.log(`You have ${playerSum} points`);
+console.log('Blackjack! You win');
 } else {
-  console.log(`You have ${playerSum} points`);
+console.log(`You have ${playerSum} points`);
 
-  // 딜러가 17 이상이면 멈추고, 17 미만이면 추가 카드 뽑음 (여기서는 이미 cardSum으로 계산)
-  if (bankSum > 21) {
-    console.log(`Bank has ${bankSum} points`);
-    console.log('Bank bust! You win');
+if (bankSum > 21) {
+  console.log(`Bank has ${bankSum} points`);
+  console.log('Bank bust! You win');
+} else {
+  console.log(`Bank has ${bankSum} points`);
+  if (playerSum > bankSum) {
+    console.log('You win');
+  } else if (playerSum < bankSum) {
+    console.log('Bank wins');
   } else {
-    console.log(`Bank has ${bankSum} points`);
-    if (playerSum > bankSum) {
-      console.log('You win');
-    } else if (playerSum < bankSum) {
-      console.log('Bank wins');
-    } else {
-      console.log('Draw');
-    }
+    console.log('Draw');
   }
+}
 }
